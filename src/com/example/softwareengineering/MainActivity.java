3547683079f03b7d658b4c0652ce2ceb2db98b09
Clown77 <mathias.schlolaut@gmail.com>
2013-06-17 19:00:58 +0200

    usercode.xml erstellt und mit Button "Erstelle deinen Benutzercode" verknüpft

--- src/com/example/softwareengineering/MainActivity.java
+++ src/com/example/softwareengineering/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.softwareengineering;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	
+	/**
+	 * soll das Fenster mit der Benutzercodegenerierung öffnen
+	 */
+	public void usercode(View view){
+		setContentView(R.layout.usercode);
 	}
 
 }
