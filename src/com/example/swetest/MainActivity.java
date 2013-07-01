2ee95ee0cd917f2fd4bce6b1f3f6496b9cdbafa1
Clown77 <mathias.schlolaut@gmail.com>
2013-07-01 20:34:28 +0200

    AlarmManager Kommentiert

--- src/com/example/swetest/MainActivity.java
+++ src/com/example/swetest/MainActivity.java
@@ -56,9 +56,11 @@ public class MainActivity extends Activity {
 	
 	public void test(View view){
 		setup();
-		setAlarm(5);
+		setAlarm(21);
 	}
 	
+	
+	//Hilfsvariablen um die Alarmzeiten zu übergeben
 	final static private long ONE_SECOND = 1000;
 	final static private long ONE_MINUTE = ONE_SECOND * 60;
 	final static private long ONE_HOUR = ONE_MINUTE * 60;
@@ -70,6 +72,9 @@ public class MainActivity extends Activity {
 
 	AlarmManager am;
 
+	/**
+	 * Legt Elemnte für AlarmManager an.
+	 */
 	private void setup() {
 
 		br = new BroadcastReceiver() {
@@ -77,7 +82,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onReceive(Context c, Intent i) {
 
-//				Toast.makeText(c, "Rise and Shine!", Toast.LENGTH_LONG).show();
+				//Definiert die Ausgabe
 				InputNotification.notify(c, "Mario", 1);
 			}
 
@@ -92,6 +97,10 @@ public class MainActivity extends Activity {
 
 	}
 
+	/**
+	 * 
+	 * @return aktuelle Tageszeit
+	 */
 	public static long getTime(){
 		long time = System.currentTimeMillis();
 		
@@ -102,6 +111,11 @@ public class MainActivity extends Activity {
 		return time;
 	}
 	
+	/**
+	 * Setz die Alarmzeit
+	 * 
+	 * @param l Uhrzeit(z.B. 15 für 15Uhr)
+	 */
 	public void setAlarm(long l) {
 		System.out.println(getTime());
 		System.out.println(l);
@@ -115,6 +129,9 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	/**
+	 * Zerstört alarmzeiten
+	 */
 	protected void onDestroy() {
 		am.cancel(pi);
 		unregisterReceiver(br);
