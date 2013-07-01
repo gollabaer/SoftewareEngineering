acaa10ac9dcd96f76f17a8af598e84d92d0480ab
Christian Bekel <c-bekel@gmx.net>
2013-07-01 20:33:37 +0200

    no message

--- src/com/example/swetest/Verwalter.java
+++ src/com/example/swetest/Verwalter.java
@@ -1,5 +1,11 @@
 package com.example.swetest;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.view.View;
+
 public class Verwalter {
 	
 	
@@ -7,7 +13,30 @@ public class Verwalter {
 	
 	
 	
-	
+	public void setUserCode(View view, String actualUsercode) {
+
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(this);
+
+		SharedPreferences.Editor editor = preferences.edit();
+
+		editor.putString("Usercode", actualUsercode);
+		editor.commit();
+
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+		alertDialog.setTitle("Succes!");
+		alertDialog.setMessage("Code erfolgreich gespeichert");
+		alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Ok",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		alertDialog.show();
+
+	}
+
 	
 	
 	
